628fae744e33e7a57411ab814eb78db898ea17e7
BOTPili <pilar.fernandez0708@gmail.com>
2022-09-03 21:14:37 -0300

    cambios

--- js/js.js
+++ js/js.js
@@ -4,21 +4,16 @@ $(function(){
     });
   })
 
-  function cambiarColor(color){
-    document.querySelector('input').value = "El color es " + color;
-    document.querySelector('.content').style.background = color;
-    document.querySelector('.input').style.background = color;
-    document.querySelector('.input').style.color = "white";
-    
-    
-    if(color == "White" || color == "Yellow"){
-        document.querySelector('.input').style.color = "black";
-    }
+  var botones = document.getElementsByTagName('button');
 
+    function cambiarColor(color){
+    document.querySelector('botonesCambioFondo').innerHTML= color;
+    document.querySelector('f0ondo').style.background = color;
     }
+    
     
      for(i=0; i < botones.length; i++){
         botones[i].onclick = function(){
-            cambiarColor(this.innerText);
+            cambiarColor(this.innerHTML);
         }
      }
