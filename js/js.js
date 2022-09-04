7b934c96a67d1da647579ce1b17f52ae02024301
BOTPili <pilar.fernandez0708@gmail.com>
2022-09-03 21:34:28 -0300

    funciona mama :+1:

--- js/js.js
+++ js/js.js
@@ -5,15 +5,13 @@ $(function(){
   })
 
   var botones = document.getElementsByTagName('button');
-
-    function cambiarColor(color){
-    document.querySelector('botonesCambioFondo').innerHTML= color;
-    document.querySelector('f0ondo').style.background = color;
-    }
-    
-    
+  
      for(i=0; i < botones.length; i++){
         botones[i].onclick = function(){
             cambiarColor(this.innerHTML);
         }
      }
+
+     function cambiarColor(color){
+        document.querySelector('botonesCambioFondo').innerHTML = document.getElementById('fondo').style.background = color; 
+    }
